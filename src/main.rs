d65c5e79e9e50d27eb99cac0c5b41c81f75215b8
Philip Müller <mail@philip.in-aachen.net>
2018-04-10 00:01:18 -0400

    experiment with parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use std::io::{self, BufRead};
 
 fn main() {
-    let stdin = io::stdin();
-    let mut iterator = stdin.lock().lines();
-    let _numbers = iterator.next().unwrap().unwrap();
-    let _target = iterator.next().unwrap().unwrap();
+  let stdin = io::stdin();
+  let mut iterator = stdin.lock().lines();
+  let numbers_string = iterator.next().unwrap().unwrap();
+  let target = iterator.next().unwrap().unwrap().parse::<u32>().unwrap();
 
-    println!("(1)")
+  let numbers = parse_numbers(&numbers_string);
+
+  println!("{:?}", numbers);
+  println!("{:?}", target);
+
+  println!("(1)")
+}
+
+fn parse_numbers(numbers: &str) -> std::vec::Vec<u32> {
+  let elements: Vec<&str> = numbers.trim_matches(|p| p == '[' || p == ']' )
+                                   .split(",")
+                                   .collect();
+
+    elements.iter().map(|e| e.parse::<u32>().unwrap()).collect()
 }
